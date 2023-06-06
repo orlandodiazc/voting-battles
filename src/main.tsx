342c6dbdb44ab1c42df9cbb4d80ed7e7f1eef03e
Orlando Diaz <orlandodiazconde@gmail.com>
2023-06-06 08:42:45 -0500

    Update font

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import "@fontsource-variable/inter";
+import "@fontsource/poppins/400.css";
+import "@fontsource/poppins/500.css";
 import { store } from "./redux/store";
 import { Provider } from "react-redux";
 if (
