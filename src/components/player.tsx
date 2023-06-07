cc30df0ddfee2e856b705a33a06fac307fa371be
Orlando Diaz <orlandodiazconde@gmail.com>
2023-06-07 13:01:10 -0500

    Update styling and selector

--- src/components/player.tsx
+++ src/components/player.tsx
@@ -6,27 +6,30 @@ export default function Player({
   breakValues,
   playerId,
   stageId,
-  values,
 }: {
   breakValues: number[];
   playerId: number;
   stageId: number;
-  values: string[];
 }) {
-  const stage = useBoardSelector((state) => state.scores[playerId][stageId]);
-  const totalPlayer =
-    stage.values.reduce((acc, curr) => acc + Number(curr), 0) +
-    (stage.extraValues?.reduce((acc, curr) => acc + (curr ? 1 : 0), 0) ?? 0);
+  const { values, extraValues } = useBoardSelector(
+    (state) => state.scores[playerId][stageId]
+  );
+  const playerTotal =
+    values.reduce((acc, curr) => acc + Number(curr), 0) +
+    (extraValues?.reduce((acc, curr) => acc + (curr ? 1 : 0), 0) ?? 0);
   return (
     <>
       {values.map((value, idx) => {
-        const padding = breakValues.some((breakIdx) => breakIdx - 1 === idx)
-          ? "pe-4"
-          : "pe-0.5";
+        const padding = breakValues.some((breakIdx) => breakIdx === idx)
+          ? "border-s-14"
+          : "border-s";
 
         const maxValue = idx >= values.length - 3 ? 2 : 4;
         return (
-          <td className={clsx(padding, "text-center")} key={idx}>
+          <td
+            className={clsx(padding, "text-center border-transparent")}
+            key={idx}
+          >
             <Input
               playerId={playerId}
               inputId={idx}
@@ -39,9 +42,9 @@ export default function Player({
       })}
       <td className="text-center align-top">
         <input
-          className="w-8 h-8 text-center bg-foreground text-background rounded disabled:bg-gray-500"
+          className="w-8 h-8 text-center text-background rounded disabled:bg-muted-foreground"
           disabled={true}
-          value={totalPlayer}
+          value={playerTotal}
         />
       </td>
     </>
