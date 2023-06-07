4048eaaef09991231eecbc79457e1441659b8fc8
Orlando Diaz <orlandodiazconde@gmail.com>
2023-06-07 12:58:10 -0500

    Extend borderWidth

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,9 @@ export default {
       sans: ["var(--font-sans)", ...fontFamily.sans],
     },
     extend: {
+      borderWidth: {
+        14: "14px",
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
