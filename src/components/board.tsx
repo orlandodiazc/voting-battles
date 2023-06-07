3dac14a217b774ed5b37ef4809197ae32e9c7262
Orlando Diaz <orlandodiazconde@gmail.com>
2023-06-07 13:00:21 -0500

    Update styling, add object destructuring

--- src/components/board.tsx
+++ src/components/board.tsx
@@ -3,39 +3,37 @@ import { useBoardDispatch, useBoardSelector } from "../hooks/hooks";
 import { RovingTabindexRoot } from "./roving-tabindex";
 import React from "react";
 import Player from "./player";
-import { setAnswer } from "../redux/slices/boardSlice";
+import { addAnswer } from "../redux/slices/boardSlice";
+import Results from "./results";
 
 export default function Board() {
   const { stages, players } = useBoardSelector((state) => state.settings);
   const scores = useBoardSelector((state) => state.scores);
-
   const dispatch = useBoardDispatch();
   return (
-    <div className="grid justify-center items-start h-[121px]">
-      {stages.map((stage, stageIdx) => {
-        const breakValues = stage.setup.map((value, idx) =>
-          stage.setup.slice(0, idx + 1).reduce((a, b) => a + b)
+    <div className="grid justify-center items-start">
+      {stages.map(({ name, setup, type }, stageIdx) => {
+        const breakValues = setup.map((value, idx) =>
+          setup.slice(0, idx + 1).reduce((a, b) => a + b)
         );
         const draftPlayers =
           stageIdx % 2 === 0 ? players : [...players].reverse();
         return (
           <TabsContent
             tabIndex={-1}
-            value={stage.name}
+            value={name}
             key={stageIdx}
-            className="relative"
+            className="overflow-x-auto h-[160px] px-5"
           >
-            <h2 className="absolute -inset-4">{stage.name}</h2>
+            <span className="-ms-2">{name}</span>
             <table className="border-separate border-spacing-0.5">
               <thead>
-                <tr className="text-[10px] text-center">
-                  <td
-                    colSpan={stage.setup.reduce((acc, curr) => acc + curr, 1)}
-                  ></td>
-                  <td>Tecnicas</td>
-                  <td>Flow</td>
-                  <td>Escena</td>
-                  <td>Total</td>
+                <tr className="text-[10px] text-center ">
+                  <th colSpan={setup.reduce((acc, curr) => acc + curr, 1)}></th>
+                  <th className="border-s-8 border-transparent">Tecnicas</th>
+                  <th>Flow</th>
+                  <th>Escena</th>
+                  <th>Total</th>
                 </tr>
               </thead>
               <RovingTabindexRoot as="tbody" active={{ rowId: 0, cellId: 0 }}>
@@ -50,11 +48,10 @@ export default function Board() {
                         playerId={playerId}
                         stageId={stageIdx}
                         breakValues={breakValues}
-                        values={scores[playerId][stageIdx].values}
                       />
                     </tr>
-                    {stage.type === "MINUTE_ANS" &&
-                      name === draftPlayers[1].name && (
+                    {type === "MINUTE_ANS" &&
+                      playerId === draftPlayers[1].id && (
                         <tr>
                           <td colSpan={1}></td>
                           {scores[playerId][stageIdx].extraValues?.map(
@@ -65,7 +62,7 @@ export default function Board() {
                                   checked={value}
                                   onChange={() =>
                                     dispatch(
-                                      setAnswer({
+                                      addAnswer({
                                         playerId,
                                         stageId: stageIdx,
                                         checkboxId: checkboxIdx,
@@ -85,6 +82,9 @@ export default function Board() {
           </TabsContent>
         );
       })}
+      <TabsContent className="overflow-x-auto h-[160px] px-5" value="Results">
+        <Results />
+      </TabsContent>
     </div>
   );
 }
