8c3331761b1d851db03ec66a97ff216d95a25193
Orlando Diaz <orlandodiazconde@gmail.com>
2023-06-08 12:19:07 -0500

    Update selector and styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
-import { useRef, useState } from "react";
+import { useCallback, useRef, useState } from "react";
 import { Tabs, TabsList, TabsTrigger } from "./components/ui/tabs";
 import { MdChevronRight, MdChevronLeft } from "react-icons/md";
 
 import { useBoardSelector } from "./hooks/hooks";
 import Board from "./components/board";
+import Settings from "./components/settings";
+import clsx from "clsx";
 
 export default function App() {
   const stageNames = useBoardSelector((state) =>
     state.settings.stages.map((stage) => stage.name)
   );
+  const players = useBoardSelector((state) => state.settings.players);
   const [value, setValue] = useState("Incremental");
   const tabListRef = useRef<HTMLDivElement | null>(null);
 
-  function getTriggers(): HTMLElement[] {
+  const getTriggers = useCallback((): HTMLElement[] => {
     if (!tabListRef.current) return [];
     return Array.from(
       tabListRef.current.querySelectorAll("[data-radix-collection-item]")
     );
-  }
+  }, [tabListRef]);
 
   function handleClick(direction: "LEFT" | "RIGHT") {
     const domTriggers = getTriggers();
@@ -30,28 +33,44 @@ export default function App() {
   }
   return (
     <main className="min-h-screen grid place-content-center">
-      <section>
-        <div>
-          <Tabs value={value} onValueChange={(value) => setValue(value)}>
-            <Board />
-            <div className="flex justify-center">
-              <button onClick={() => handleClick("LEFT")}>
-                <MdChevronLeft size={26} />
-              </button>
-              <TabsList ref={tabListRef}>
-                {stageNames.map((name, idx) => (
-                  <TabsTrigger value={name} key={idx}>
-                    {idx}
-                  </TabsTrigger>
-                ))}
-                <TabsTrigger value="Results">R</TabsTrigger>
-              </TabsList>
-              <button onClick={() => handleClick("RIGHT")}>
-                <MdChevronRight size={26} />
-              </button>
+      <Tabs value={value} onValueChange={(value) => setValue(value)} asChild>
+        <section className="w-[560px]">
+          <div className="flex justify-between bg-muted px-2">
+            <button onClick={() => handleClick("LEFT")} tabIndex={-1}>
+              <MdChevronLeft size={26} />
+            </button>
+            <TabsList ref={tabListRef} tabIndex={-1}>
+              {stageNames.map((name, idx) => (
+                <TabsTrigger value={name} key={idx}>
+                  {name}
+                </TabsTrigger>
+              ))}
+              <TabsTrigger value="Results" className="text-yellow-600">
+                R
+              </TabsTrigger>
+            </TabsList>
+            <button onClick={() => handleClick("RIGHT")} tabIndex={-1}>
+              <MdChevronRight size={26} />
+            </button>
+          </div>
+          <div className="h-40">
+            <Board tabValue={value} />
+            <div
+              className={clsx(
+                "flex justify-center gap-2 text-sm",
+                value === "Results" && "hidden"
+              )}
+            >
+              <span>TOTAL</span>
+              {players.map(({ name, id }) => (
+                <span key={id}>{name}:</span>
+              ))}
             </div>
-          </Tabs>
-        </div>
+          </div>
+        </section>
+      </Tabs>
+      <section>
+        <Settings />
       </section>
     </main>
   );
