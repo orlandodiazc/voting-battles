5fc3c49e862b01734bef69dff6a899ed67c947e8
Orlando Diaz <orlandodiazconde@gmail.com>
2023-06-09 07:48:38 -0500

    Add usePlayersTotal

--- src/hooks/hooks.ts
+++ src/hooks/hooks.ts
@@ -1,7 +1,22 @@
-import { useDispatch, useSelector } from "react-redux";
 import type { TypedUseSelectorHook } from "react-redux";
-import type { RootState, AppDispatch } from "../redux/store";
+import { useDispatch, useSelector } from "react-redux";
+
+import type { AppDispatch, RootState } from "../redux/store";
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useBoardDispatch: () => AppDispatch = useDispatch;
 export const useBoardSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+export function usePlayersTotal() {
+	const scores = useBoardSelector((state) => state.scores);
+	const playerScores = scores.map((player) =>
+		player.reduce(
+			(acc, { values, extraValues }) =>
+				acc +
+				values.reduce((acc, curr) => acc + Number(curr), 0) +
+				(extraValues?.reduce((acc, curr) => acc + (curr ? 1 : 0), 0) ?? 0),
+			0
+		)
+	);
+	return playerScores;
+}
